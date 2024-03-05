33aa20aa66ed8edb76cf26d2bc195ad800739f8e
kaganuk <kagankilicli@gmail.com>
2024-03-05 11:54:30 +0100

    Remove id assignment for developers in initial data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 -- TODO: implement initial data for test purposes
-INSERT INTO DEVELOPER (id, name) VALUES
-  (1, 'Developer 1'),
-  (2, 'Developer 2'),
-  (3, 'Developer 3'),
-  (4, 'Developer 4');
+INSERT INTO DEVELOPER (name) VALUES
+  ('Developer 1'),
+  ('Developer 2'),
+  ('Developer 3'),
+  ('Developer 4');
