0e99aa8f53e0fc0b7df0cc4bbda802715cd73490
kaganuk <kagankilicli@gmail.com>
2024-03-05 11:12:46 +0100

    Add some developers for initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+-- TODO: implement initial data for test purposes
+INSERT INTO DEVELOPER (id, name) VALUES
+  (1, 'Developer 1'),
+  (2, 'Developer 2'),
+  (3, 'Developer 3'),
+  (4, 'Developer 4');
